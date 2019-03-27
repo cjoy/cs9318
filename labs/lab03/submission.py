5e190ba8c3f5628f713a529cb51eac814bc496ee
Chris Joy <chrisjoydev@gmail.com>
2019-03-28 00:14:34 +1100

    Incremental progress on lab 3

--- labs/lab03/submission.py
+++ labs/lab03/submission.py
@@ -1,12 +1,21 @@
 import numpy as np
 import pandas as pd
 
+def sigmoid(x):
+  return 1/(1+np.exp(-x))
 
-def logistic_regression(data, labels, weights, num_epochs, learning_rate): # do not change the heading of the function
-  pass
+def logistic_regression(data, labels, weights, num_epochs, learning_rate):
+  X = np.c_[np.ones(data.shape[0]), data]
+  y = labels
+  W = weights
 
+  for _ in range(num_epochs):
+    z = np.dot(X, W)
+    h = sigmoid(z)
+    gradient = np.dot(X.T, (y - h))
+    W += learning_rate * gradient
 
-
+  return W
 
 if __name__ == '__main__':
   data_file='./asset/a'
@@ -15,12 +24,9 @@ if __name__ == '__main__':
   labels=raw_data['Label'].values
   data=np.stack((raw_data['Col1'].values,raw_data['Col2'].values), axis=-1)
 
-  np.set_printoptions(threshold=np.nan)
-  print(labels)
-  # print(data)
-
   ## Fixed Parameters. Please do not change values of these parameters...
   weights = np.zeros(3) # We compute the weight for the intercept as well...
+  # num_epochs = 5000000
   num_epochs = 50000
   learning_rate = 50e-5
 
